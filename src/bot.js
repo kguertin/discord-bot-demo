1f94bc44ba66edb1b23382fc722cc1f470da1f92
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-07 00:16:58 -0400

    add ready event

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,10 @@ const { Client } = require('discord.js');
 
 const client = new Client();
 
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in!`)  
+})
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
 
