df865e03d36ffa81d53b0baeaab95f8a03b84a97
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-08 20:55:28 -0400

    send resonse from bot

--- src/bot.js
+++ src/bot.js
@@ -8,6 +8,14 @@ client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`)  
 })
 
+client.on('message', (message) =>{
+    if(message.author.bot) return;
+    console.log(`${message.author.tag}: ${message.content}`)
+    if(message.content === 'hello'){
+        message.channel.send('hello')
+    }
+})
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
 
