3fd2ca7683c6d4e8625529c397cb5ab3407190b0
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-10-10 20:08:10 -0400

    learn how to handle basic commands

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 
 const client = new Client();
+const PREFIX = '$';
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in!`)  
@@ -10,9 +11,13 @@ client.on('ready', () => {
 
 client.on('message', (message) =>{
     if(message.author.bot) return;
-    console.log(`${message.author.tag}: ${message.content}`)
-    if(message.content === 'hello'){
-        message.channel.send('hello')
+    if(message.content.startsWith(PREFIX)){
+        const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+        console.log(CMD_NAME);
+        console.log(args);
     }
 })
 
